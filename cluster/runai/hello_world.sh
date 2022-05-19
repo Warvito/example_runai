97ca16b9d2d4b436e4f32e11358736559bee52e4
Warvito <ianonimato@hotmail.com>
2022-05-19 14:00:56 +0100

    Created submit command

---
+++ cluster/runai/hello_world.sh
@@ -0,0 +1,12 @@
+runai submit \
+  --name test-example \
+  --image 10.202.67.207:5000/wds20:kch_eval \
+  --backoff-limit 0 \
+  --gpu 1 \
+  --cpu 4 \
+  --large-shm \
+  --run-as-user \
+  --host-ipc \
+  --project wds20 \
+  --volume /nfs/home/wds20/projects/example_runai:/project/ \
+  --command -- python3 /project/src/test.py
